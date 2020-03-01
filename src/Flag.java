37d7f7b6fc2f0b45370956a88c24f2681cda4c21
yourUsername <yourEmail@domain.com>
2020-03-01 18:00:55 -0500

    fF

---
+++ src/Flag.java
@@ -0,0 +1,17 @@
+public class Flag {
+
+public static void main (String[]args) {
+
+    System.out.println("* * *  ================="  );
+    System.out.println("* * *  =================" );
+    System.out.println("* * *  =================" );
+    System.out.println("========================" );
+    System.out.println("========================" );
+    System.out.println("========================" );
+
+
+
+
+}
+
+}
